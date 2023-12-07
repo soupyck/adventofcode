9b03ce2a34b660e7e8d1b8c128d0c1d978bc6534
Eric Campbell <eric@kootenaicreative.com>
2023-12-07 08:35:04 -0800

    fix reged

--- Day.ps1
+++ Day.ps1
@@ -3,16 +3,16 @@ $elfinput = Get-Content .\day1.txt
 
 foreach ($line in $elfinput)
 {
-    $newline = $line -replace '\D+([0-9]*).*', '$1'
+    $newline = $line -replace '[^\d]', ''
     if ($newline.Length -ge 2)
     {
-        $fullline = $newline[0] + $newline[1]
+        $fullline = $newline[0] + $newline[-1]
     }
     elseif ($newline.Length -eq 1)
     {
         $fullline = $newline[0] + $newline[0]
     }
-    Write-Debug "Adding $fullline to $sum"    
+    #Write-Host "Adding $fullline to $sum"    
     $sum = ([int]$fullline + $sum)
 }
 
