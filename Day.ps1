c9144281107bdde3ac0efd850a79b91dc19047db
Eric Campbell <eric@kootenaicreative.com>
2023-12-07 08:17:33 -0800

    fixed additive logic

--- Day.ps1
+++ Day.ps1
@@ -6,14 +6,14 @@ foreach ($line in $elfinput)
     $newline = $line -replace '\D+([0-9]*).*', '$1'
     if ($newline.Length -ge 2)
     {
-        $linesum = [int]$newline[0] + [int]$newline[1]
+        $fullline = $newline[0] + $newline[1]
     }
     elseif ($newline.Length -eq 1)
     {
-        $linesum = [int]$newline[0] + [int]$newline[0]
+        $fullline = $newline[0] + $newline[0]
     }
-    $sum += $linesum
-    Write-Host $sum
+    Write-Debug "Adding $fullline to $sum"    
+    $sum = ([int]$fullline + $sum)
 }
 
-#$sum
+Write-Host "The Total is $sum"
